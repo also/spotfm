71009e26d0799fee532eae0b3f636f5e63d8fb07
Reid Draper <reiddraper@reid.local>
2010-10-16 17:36:23 -0400

    listen to events on the window so we can refresh when the dom changes

--- chrome-extension/logic.js
+++ chrome-extension/logic.js
@@ -1,12 +1,11 @@
-var className = ".preview_icon";
-var body = $$('table.candyStriped tbody')[0];
+document.window.addEvent('click', function(theEvent) {
+    var target = theEvent.target;
+    var parents = target.getParents('.candyStriped');
+    if (parents.length !== 0){
 
-body.getChildren().each(function (item){
-    item.addEvent('click', function(){
-        console.log('click!');
         // get the name of the track
-        var subjectCell = this.getChildren('td.subjectCell');
-        var text = subjectCell.get('text')[0];
+        var subjectCell = target.getParents('td.subjectCell')[0];
+        var text = subjectCell.get('text');
 
         var urlElement = subjectCell.getElements('a');
         var url = urlElement[0].get('href');
@@ -19,16 +18,15 @@ body.getChildren().each(function (item){
         var requestURL = 'http://ws.spotify.com/search/1/track.json?q=' + query;
         var jsonRequest = new Request.JSON({url: requestURL, method:'get', headers: {}, onSuccess: function(json_object, json_string){
             var track = json_object.tracks[0];
-            console.log([track.artists[0].name, track.album.name, track.name].join(' '));
+            //console.log([track.artists[0].name, track.album.name, track.name].join(' '));
             var id = track.href;
             console.log(id);
-            //var audioRequest = new Request({url: "localhost:9999/play/" + id, method:'get'})
-            var audioRequest = new Request({url: "http://10.10.11.221:9999/play/" + id, method:'get'})
+            var audioRequest = new Request({url: "http://localhost:9999/play/" + id, method:'get'});
             audioRequest.send();
             }
         });
         jsonRequest.send();
-        }
-    );
     }
-);
+});
+
+
