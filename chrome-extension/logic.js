6c6313843484e23febb1586198576574c537df7a
Reid Draper <reiddraper@reid.local>
2010-10-16 13:25:20 -0400

    replace play buttons with text that says 'yo'

--- chrome-extension/logic.js
+++ chrome-extension/logic.js
@@ -1,3 +1,9 @@
 var className = ".preview_icon";
 var previewIcons = $$(className);
-previewIcons.each(function (item) { item.destroy() });
+
+previewIcons.each(function (item){
+    var newElement = new Element('span')
+    newElement.set('text', 'yo');
+    item.getParent().getParent().grab(newElement);
+    item.destroy(); 
+    });
