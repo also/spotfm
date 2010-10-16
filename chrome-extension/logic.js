96b38041935caa83b6a4d18a812f160e48206770
Reid Draper <reiddraper@reid.local>
2010-10-16 16:06:07 -0400

    fix problem with css on certain last.fm pages

--- chrome-extension/logic.js
+++ chrome-extension/logic.js
@@ -8,7 +8,8 @@ body.getChildren().each(function (item){
         var subjectCell = this.getChildren('td.subjectCell');
         var text = subjectCell.get('text')[0];
 
-        var url = subjectCell.getChildren('a')[0].get('href');
+        var urlElement = subjectCell.getElements('a');
+        var url = urlElement[0].get('href');
         var artist = url.toString().split('/')[2];
         artist = unescape(artist);
         artist = artist.replace(/\+/g, ' ');
@@ -18,7 +19,7 @@ body.getChildren().each(function (item){
         var requestURL = 'http://ws.spotify.com/search/1/track.json?q=' + query;
         var jsonRequest = new Request.JSON({url: requestURL, method:'get', headers: {}, onSuccess: function(json_object, json_string){
             var track = json_object.tracks[0];
-            //console.log([track.artists[0].name, track.album.name, track.name].join(' '));
+            console.log([track.artists[0].name, track.album.name, track.name].join(' '));
             var id = track.href;
             console.log(id);
             //var audioRequest = new Request({url: "localhost:9999/play/" + id, method:'get'})
