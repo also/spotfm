42d89020294413ff974c21df25d86cbb33fdf5ce
Reid Draper <reiddraper@reid.local>
2010-10-16 15:49:19 -0400

    remove the empty icon, request the audio to start playing

--- chrome-extension/logic.js
+++ chrome-extension/logic.js
@@ -3,6 +3,7 @@ var body = $$('table.candyStriped tbody')[0];
 
 body.getChildren().each(function (item){
     item.addEvent('click', function(){
+        console.log('click!');
         // get the name of the track
         var subjectCell = this.getChildren('td.subjectCell');
         var text = subjectCell.get('text')[0];
@@ -16,7 +17,13 @@ body.getChildren().each(function (item){
         var query = [escape(artist), escape(text)].join('+');
         var requestURL = 'http://ws.spotify.com/search/1/track.json?q=' + query;
         var jsonRequest = new Request.JSON({url: requestURL, method:'get', headers: {}, onSuccess: function(json_object, json_string){
-            console.log(json_string);
+            var track = json_object.tracks[0];
+            //console.log([track.artists[0].name, track.album.name, track.name].join(' '));
+            var id = track.href;
+            console.log(id);
+            //var audioRequest = new Request({url: "localhost:9999/play/" + id, method:'get'})
+            var audioRequest = new Request({url: "http://10.10.11.221:9999/play/" + id, method:'get'})
+            audioRequest.send();
             }
         });
         jsonRequest.send();
