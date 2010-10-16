7af43ce8fc27b74f733cd4f7acd9b88e60a4c661
Reid Draper <reiddraper@reid.local>
2010-10-16 15:30:38 -0400

    make requests to spotify on click

--- chrome-extension/logic.js
+++ chrome-extension/logic.js
@@ -1,18 +1,25 @@
 var className = ".preview_icon";
-var body = $$('table.candyStriped tbody')[0]
+var body = $$('table.candyStriped tbody')[0];
 
 body.getChildren().each(function (item){
     item.addEvent('click', function(){
         // get the name of the track
         var subjectCell = this.getChildren('td.subjectCell');
-        var text = subjectCell.get('text');
+        var text = subjectCell.get('text')[0];
 
         var url = subjectCell.getChildren('a')[0].get('href');
-        var artist = url.toString().split('/')[2]
+        var artist = url.toString().split('/')[2];
         artist = unescape(artist);
         artist = artist.replace(/\+/g, ' ');
         artist = unescape(artist);
-        alert(artist);
+
+        var query = [escape(artist), escape(text)].join('+');
+        var requestURL = 'http://ws.spotify.com/search/1/track.json?q=' + query;
+        var jsonRequest = new Request.JSON({url: requestURL, method:'get', headers: {}, onSuccess: function(json_object, json_string){
+            console.log(json_string);
+            }
+        });
+        jsonRequest.send();
         }
     );
     }
