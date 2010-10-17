6036091834e961078353c5b6f5dbed27d5836095
Reid Draper <reiddraper@reid.local>
2010-10-17 13:49:52 -0400

    set up a skeleton for the websockets

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,1 +1,23 @@
-chrome.browserAction.onClicked.addListener(function() {alert('hey');});
+var play = true;
+//var ws = new WebSocket('ws://localhost:9998/');
+
+/*
+ws.onmessage = function(message){
+    var data = message.data;
+    play = data;
+    chrome.browserAction.setIcon({path:getImage(play)});
+}
+*/
+
+function getImage(state){
+    if (state){
+        return "play.jpeg";
+    }
+    else {
+        return "pause.jpeg";
+    }
+}
+chrome.browserAction.onClicked.addListener(function() {
+    play = !play; 
+    chrome.browserAction.setIcon({path:getImage(play)});
+});
