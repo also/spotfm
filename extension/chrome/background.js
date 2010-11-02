94abd7e0165bce53b612154a5574242a0206205f
Reid Draper <reiddraper@reid.local>
2010-11-01 21:37:03 -0400

    set up some logging for the websocket calls

--- extension/chrome/background.js
+++ extension/chrome/background.js
@@ -1,11 +1,19 @@
-/*
 var play = true;
-//var ws = new WebSocket('ws://localhost:9998/');
+var ws = new WebSocket('ws://localhost:9999/monitor');
 
 ws.onmessage = function(message){
     var data = message.data;
-    play = data;
-    chrome.browserAction.setIcon({path:getImage(play)});
+    console.log(message.data);
+    //play = data;
+    //chrome.browserAction.setIcon({path:getImage(play)});
+}
+
+ws.onopen = function(open){
+    console.log('the web socket was opened');
+}
+
+ws.onclose = function(close){
+    console.log('the web socket was closed');
 }
 
 function getImage(state){
@@ -17,8 +25,6 @@ function getImage(state){
     }
 }
 chrome.browserAction.onClicked.addListener(function() {
-    play = !play; 
-    chrome.browserAction.setIcon({path:getImage(play)});
+//    play = !play; 
+//    chrome.browserAction.setIcon({path:getImage(play)});
 });
-
-*/
