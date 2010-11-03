4f9360b556b92cee58585bd5814195599b1014fa
Ryan Berdeen <ryan@ryanberdeen.com>
2010-11-02 21:40:35 -0400

    remove spotify icon if unresolvable; improve icon display

--- extension/common/lastfm.js
+++ extension/common/lastfm.js
@@ -4,7 +4,8 @@ document.window.addEvent('click', function(theEvent) {
     if (parents.length !== 0) {
         // get the name of the track
         var trElt = target.getParent('tr');
-        var urlElement = trElt.getChildren('td.subjectCell a')[0];
+        // get the last link in the cell (if more than one, first is artist)
+        var urlElement = trElt.getChildren('td.subjectCell a').pop();
         var url = urlElement.get('href');
         var artist = url.toString().split('/')[2];
         artist = unescape(artist);
@@ -17,6 +18,14 @@ document.window.addEvent('click', function(theEvent) {
         text = unescape(text);
         console.log(text);
 
-        spotfm.resolveAndPlay({artist: artist, track: text});
+        spotfm.resolveAndPlay({artist: artist, track: text}, {
+            onResolutionFailure: function () {
+                console.log("resolution failed");
+                // TODO without adding a class, the data attribute doesn't seem
+                // to be noticed until the mouse moves out of the row
+                trElt.addClass('thunk');
+                trElt.setProperty('data-spotify-unresolvable', 'true');
+            }
+        });
     }
 });
--- extension/common/tab.js
+++ extension/common/tab.js
@@ -4,20 +4,23 @@ var spotfm = {
         audioRequest.send();
     },
 
-    resolveAndPlay: function (trackInfo) {
+    resolveAndPlay: function (trackInfo, options) {
+        options = options || {};
         var query = [escape(trackInfo.artist), escape(trackInfo.track)].join('+');
             var requestURL = 'http://ws.spotify.com/search/1/track.json?q=' + query;
             console.log(requestURL);
             var jsonRequest = new Request.JSON({url: requestURL, method:'get', headers: {}, onSuccess: function(json_object, json_string){
                 var tracks = json_object.tracks;
-                if (tracks.length !== 0){
+                if (tracks.length !== 0) {
                     var track = tracks[0];
                     var id = track.href;
                     console.log([track.artists[0].name, track.album.name, track.name].join(' '));
                     spotfm.play(id);
                 }
                 else {
-                    console.log("couldn't resolve");
+                    if (options.onResolutionFailure) {
+                        options.onResolutionFailure();
+                    }
                 }
             }
         });
