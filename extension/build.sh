a8dae9e1c0fa547a7b500deff5290cd5bcc00492
Ryan Berdeen <ryan@ryanberdeen.com>
2010-11-09 18:29:35 -0500

    clean up extension build process

--- extension/build.sh
+++ extension/build.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-cp common/* chrome
-cp common/* spotfm.safariextension
+mkdir -p build build/chrome build/spotfm.safariextension
 
-cp ../js/* chrome
-cp ../js/* spotfm.safariextension
+cp chrome/* build/chrome
+cp spotfm.safariextension/* build/spotfm.safariextension
+
+cp common/* build/chrome
+cp common/* build/spotfm.safariextension
+
+cp ../js/* build/chrome
+cp ../js/* build/spotfm.safariextension
