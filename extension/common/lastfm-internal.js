b11566349fa1aceffc73a74c8cb9b1f6ea1110a4
Ryan Berdeen <ryan@ryanberdeen.com>
2011-01-02 17:55:18 -0500

    don't clutter last.fm page scope

--- extension/common/lastfm-internal.js
+++ extension/common/lastfm-internal.js
@@ -1,5 +1,7 @@
+(function () {
 document.body.setAttribute('data-session', JSON.stringify(LFM.Session));
 
 var event = document.createEvent('Event');
 event.initEvent('lastfmSession', true, true);
 document.dispatchEvent(event);
+})();
