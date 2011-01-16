bc08cb92d82669fbd616479a5feb13fc458f46a5
Ryan Berdeen <ryan@ryanberdeen.com>
2011-01-15 21:21:38 -0500

    reorganize

--- extension/build.sh
+++ extension/build.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
-
 rm -rf build
 
-mkdir -p build build/chrome build/spotfm.safariextension
+for b in {chrome,safari}
+do
+    mkdir -p build build/$b/ext
+    cp ext/$b/* build/$b/ext
+    cp ext/common/* build/$b/ext
+    
+    cp -r browse build/$b
+    cp -r omnifm build/$b
+    cp -r source build/$b
+    
+    cp common/* build/$b
+    
+    cp $b/* build/$b
+    
+    cp ../js/omnifm-core.js build/$b/omnifm
+    cp ../js/spotfm-core.js build/$b/source/spotfm
+    cp ../js/jquery-1.4.4.min.js build/$b
+done
 
-cp chrome/* build/chrome
-cp spotfm.safariextension/* build/spotfm.safariextension
-
-cp common/* build/chrome
-cp common/* build/spotfm.safariextension
-
-cp ../js/* build/chrome
-cp ../js/* build/spotfm.safariextension
+mv build/safari build/spotfm.safariextension
